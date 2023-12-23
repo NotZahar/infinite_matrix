94635b1232fffa5f610af29cd32e2518146a5f6f
Nazar Glukhov <nazar.glukhov@gmail.com>
2023-12-23 19:41:15 +0300

    matrix logic base

--- libs/InfiniteMatrix/include/matrix.hpp
+++ libs/InfiniteMatrix/include/matrix.hpp
@@ -1,13 +1,86 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <cstddef>
+#include <iostream>
 #include <vector>
+#include <map>
+#include <memory>
 
 #include "helper.hpp"
 
 namespace matrix {
+    template <class T>
+    class MatrixData {
+    public:
+        MatrixData() = delete;
+        MatrixData(std::shared_ptr<std::map<Point, MatrixData<T>>> matrix)
+            : _matrix(matrix)
+        {}
+
+        MatrixData& operator=(T value) {
+            // if (value == T()) {
+            //     auto it = matrix_ref.sparse_matrix.find(std::make_pair(row, col));
+            //     if (it != matrix_ref.sparse_matrix.end()) {
+            //         matrix_ref.sparse_matrix.erase(it);
+            //     }
+            //     } else {
+            //     matrix_ref.sparse_matrix[std::make_pair(row, col)] = value;
+            // }
+            _data = value;
+            return *this;
+        }
+
+        operator T() const {
+            return _data;
+        }
+
+    private:
+        T _data;
+        std::weak_ptr<std::map<Point, MatrixData<T>>> _matrix;
+    };
+
+    template <class T>
+    class PMatrix {
+    public:
+        PMatrix() = delete;
+        PMatrix(size_t row, std::shared_ptr<std::map<Point, MatrixData<T>>> matrix)
+            : _row(row),
+              _matrix(matrix)
+        {}
+
+        MatrixData<T> operator[](size_t column) {
+            std::cout << "MatrixData<T>& operator[](size_t column)\n";
+            if (_matrix->contains({ _row, column })) {
+                return _matrix->at({ _row, column }); 
+            }
+
+            return MatrixData<T>(_matrix);
+        }
+
+    private:
+        size_t _row;
+        std::shared_ptr<std::map<Point, MatrixData<T>>> _matrix;
+    };
+    
+    template <class T>
     class Matrix {
-        
+    public:
+        Matrix() 
+            : _matrix(std::make_shared<std::map<Point, MatrixData<T>>>())
+        {}
+
+        PMatrix<T> operator[](size_t row) {
+            std::cout << "PMatrix<T> operator[](size_t row)\n";
+            return PMatrix<T>(row, _matrix); 
+        }
+
+        size_t size() const {
+            return _matrix->size();
+        }
+
+    private:
+        std::shared_ptr<std::map<Point, MatrixData<T>>> _matrix;
     };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
+#include <iostream>
+
 #include <matrix.hpp>
 
+#define THROW_IF(expr, msg) if(expr) { std::cout << msg << std::endl; throw; }
+
 int main(int, char**) {
-    [[maybe_unused]] matrix::Matrix matrix;
+    matrix::Matrix<int> matrix;
+    std::cout << matrix.size() << std::endl;
+    // THROW_IF(matrix.size() != 0, "matrix.size() != 0");
+
+    auto a = matrix[0][0];
+    std::cout << a << "\n";
+    // THROW_IF(a != int(), "a != int()");
+    // THROW_IF(matrix.size() != 0, "13, matrix.size() != 0");
+
+    // matrix[{100, 100}] = 314;
+    // if(matrix[{100, 100}] != 314) {
+    //     std::cout << "matrix[{100, 100}] != 314" << std::endl;
+    //     throw;
+    // }
+    // THROW_IF(matrix.size() != 1, "matrix.size() != 1");
+    // // выведется одна строка
+    // // 100100314
+    // for (auto it = matrix.cbegin(); it != matrix.cend(); ++it) {
+    //     std::cout << it->first.x << " " << it->first.y << " " << it->second << std::endl;
+    // }
     
     return 0;
 }
